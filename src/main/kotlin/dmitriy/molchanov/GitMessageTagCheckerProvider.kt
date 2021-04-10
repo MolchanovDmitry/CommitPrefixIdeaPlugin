12ffea5ceaf037fb169cc284453f279d4fdd5860
dmolchanov <dmolchanov@uma.tech>
2021-04-10 10:10:15 +0300

    release 1.3 final gradle migrate

--- src/main/kotlin/dmitriy/molchanov/GitMessageTagCheckerProvider.kt
+++ src/main/kotlin/dmitriy/molchanov/GitMessageTagCheckerProvider.kt
@@ -3,9 +3,9 @@ package dmitriy.molchanov
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.LocalChangeList
 import com.intellij.openapi.vcs.changes.ui.CommitMessageProvider
+import dmitriy.molchanov.data.Repository
 import git4idea.repo.GitRepository
 import git4idea.repo.GitRepositoryManager
-import dmitriy.molchanov.data.Repository
 
 class GitMessageTagCheckerProvider : CommitMessageProvider {
 
@@ -17,27 +17,27 @@ class GitMessageTagCheckerProvider : CommitMessageProvider {
         val regex = getRegexForRepository(currentRepository) ?: return lastComment
         val match = regex.find(branchName)
         return match?.value
-                ?.let { taskPrefix -> getConcatenatedMessage(lastComment, taskPrefix, regex) }
-                ?: lastComment
+            ?.let { taskPrefix -> getConcatenatedMessage(lastComment, taskPrefix, regex) }
+            ?: lastComment
     }
 
     private fun getRegexForRepository(repository: GitRepository): Regex? {
         val remoteUrl = repository
-                .info.remotes.firstOrNull()
-                ?.firstUrl
-                ?: return null
+            .info.remotes.firstOrNull()
+            ?.firstUrl
+            ?: return null
         return Repository.instance
-                .getRules()
-                .firstOrNull { it.gitRepo.trim() == remoteUrl.trim() }
-                ?.regexPrefix
-                ?.let(::Regex)
+            .getRules()
+            .firstOrNull { it.gitRepo.trim() == remoteUrl.trim() }
+            ?.regexPrefix
+            ?.let(::Regex)
     }
 
     private fun getConcatenatedMessage(lastComment: String?, taskPrefix: String, regex: Regex): String {
         if (lastComment.isNullOrEmpty()) return "$taskPrefix "
         val currentPrefix = regex.find(lastComment)?.value
         return currentPrefix
-                ?.let { lastComment.replace(currentPrefix, taskPrefix) }
-                ?: "$taskPrefix $lastComment"
+            ?.let { lastComment.replace(currentPrefix, taskPrefix) }
+            ?: "$taskPrefix $lastComment"
     }
 }
--- src/main/kotlin/dmitriy/molchanov/ui/main/SettingsDialog.kt
+++ src/main/kotlin/dmitriy/molchanov/ui/main/SettingsDialog.kt
@@ -6,11 +6,18 @@ import com.intellij.ui.SizedIcon
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.table.JBTable
 import dmitriy.molchanov.Strings
+import dmitriy.molchanov.Strings.GITHUB_URL
+import dmitriy.molchanov.Strings.LIKE_THIS_PLUGIN
+import dmitriy.molchanov.Strings.STAR_ON_GITHUB
 import dmitriy.molchanov.model.Rule
 import org.jdesktop.swingx.HorizontalLayout
 import org.jdesktop.swingx.VerticalLayout
+import java.awt.Cursor
+import java.awt.Desktop
 import java.awt.Dimension
+import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
+import java.net.URI
 import javax.swing.Icon
 import javax.swing.JComponent
 import javax.swing.JLabel
@@ -19,7 +26,7 @@ import javax.swing.table.DefaultTableModel
 
 
 class SettingsDialog(
-        private val listener: OnSettingsDialogListener
+    private val listener: OnSettingsDialogListener
 
 ) : DialogWrapper(true) {
 
@@ -50,14 +57,15 @@ class SettingsDialog(
     }
 
     fun getSelectedRules() = table.selectedRows
-            .map {
-                val repo = tableModel.getValueAt(it, 0) as String
-                val regexPrefix = tableModel.getValueAt(it, 1) as String
-                val checkString = tableModel.getValueAt(it, 2) as String
-                Rule(repo, regexPrefix, checkString)
-            }.toList()
+        .map {
+            val repo = tableModel.getValueAt(it, REP_INDEX) as String
+            val regexPrefix = tableModel.getValueAt(it, REGEX_INDEX) as String
+            val checkString = tableModel.getValueAt(it, CHECK_INDEX) as String
+            Rule(repo, regexPrefix, checkString)
+        }.toList()
 
     override fun createCenterPanel(): JComponent {
+        val root = JPanel(VerticalLayout())
         val dialogPanel = JPanel(HorizontalLayout())
 
         table = getTable()
@@ -66,7 +74,10 @@ class SettingsDialog(
 
         dialogPanel.add(scrollPane)
         dialogPanel.add(rightPanel)
-        return dialogPanel
+        root.add(dialogPanel)
+        root.add(getBottomLink())
+
+        return root
     }
 
     private fun getTable(): JBTable {
@@ -96,6 +107,27 @@ class SettingsDialog(
         return label
     }
 
+    private fun getBottomLink() =
+        JPanel(HorizontalLayout()).apply {
+            val label = JLabel(LIKE_THIS_PLUGIN)
+            val link = JLabel("<html><a href='$GITHUB_URL'><font color=#5597EB>$STAR_ON_GITHUB</font></a></html>")
+            link.addMouseListener(object : MouseAdapter() {
+                override fun mouseClicked(e: MouseEvent?) {
+                    Desktop.getDesktop().browse(URI(GITHUB_URL))
+                }
+
+                override fun mouseEntered(e: MouseEvent?) {
+                    cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
+                }
+
+                override fun mouseExited(e: MouseEvent?) {
+                    cursor = Cursor.getDefaultCursor()
+                }
+            })
+            add(label)
+            add(link)
+        }
+
     interface OnSettingsDialogListener {
         fun onAddClick()
         fun onRemoveClick()
@@ -104,5 +136,8 @@ class SettingsDialog(
 
     private companion object {
         const val ICON_SIZE = 25
+        const val REP_INDEX = 0
+        const val REGEX_INDEX = 1
+        const val CHECK_INDEX = 2
     }
 }
